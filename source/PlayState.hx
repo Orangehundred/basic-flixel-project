3f4b854af5428b044843abf9b6df91b9066199b2
Orangehundred <87043291+Orangehundred@users.noreply.github.com>
2022-02-13 04:32:42 -0600

    Final Commit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,8 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+		var hero:Hero = new Hero(300,300);
+		add(hero);
 	}
 
 	override public function update(elapsed:Float)
